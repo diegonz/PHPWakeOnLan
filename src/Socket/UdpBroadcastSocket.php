c2fa7c1660ef26b88eb60381a20b5e515ad36c14
Diego González <7355149+diegonz@users.noreply.github.com>
2019-04-01 21:03:45 +0200

    Updated CHANGELOG Minor code cleaning

--- src/Socket/UdpBroadcastSocket.php
+++ src/Socket/UdpBroadcastSocket.php
@@ -2,6 +2,8 @@
 
 namespace Diegonz\PHPWakeOnLan\Socket;
 
+use RuntimeException;
+
 /**
  * Class UdpBroadcastSocket.
  */
@@ -13,9 +15,9 @@ class UdpBroadcastSocket extends Socket
     public function __construct()
     {
         parent::__construct(SOL_UDP);
-        $optionResult = \socket_set_option($this->socket, SOL_SOCKET, SO_BROADCAST, true);
+        $optionResult = socket_set_option($this->socket, SOL_SOCKET, SO_BROADCAST, true);
         if (! $optionResult) {
-            throw new \RuntimeException('Error: Could not set broadcast UDP socket.', 5);
+            throw new RuntimeException('Error: Could not set broadcast UDP socket.', 5);
         }
     }
 }
