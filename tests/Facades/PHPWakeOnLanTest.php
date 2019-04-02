47b8ea632258e49ce46152abb16f2d3a9c4d97ac
Diego <dgonzalez.bz@gmail.com>
2019-04-02 23:02:49 +0200

    Updated README Removed route, view and controller Minor code cleaning Added dummy facade test

---
+++ tests/Facades/PHPWakeOnLanTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Diegonz\PHPWakeOnLan\Tests\Facades;
+
+use Diegonz\PHPWakeOnLan\Facades\PHPWakeOnLan;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PHPWakeOnLanTest
+ *
+ * @covers \Diegonz\PHPWakeOnLan\Facades\PHPWakeOnLan
+ */
+class PHPWakeOnLanTest extends TestCase
+{
+
+    /**
+     * @covers \Diegonz\PHPWakeOnLan\Facades\PHPWakeOnLan::getFacadeAccessor()
+     */
+    public function testGetFacadeAccessor(): void
+    {
+        PHPWakeOnLan::shouldReceive('getFacadeAccessor')->once()->andReturn('php-wake-on-lan');
+
+        $this->assertEquals('php-wake-on-lan', PHPWakeOnLan::__callStatic('getFacadeAccessor', []));
+    }
+}
--- tests/PHPWakeOnLanTest.php
+++ tests/PHPWakeOnLanTest.php
@@ -6,16 +6,31 @@ use PHPUnit\Framework\TestCase;
 use Diegonz\PHPWakeOnLan\PHPWakeOnLan;
 
 /**
- * Class PHPWakeOnLanTest.
+ * Class PHPWakeOnLanTest
+ *
+ * @covers \Diegonz\PHPWakeOnLan\PHPWakeOnLan
  */
 class PHPWakeOnLanTest extends TestCase
 {
     /**
      * @covers \Diegonz\PHPWakeOnLan\PHPWakeOnLan::isBroadcastAddressValid()
      */
-    public function testIsBroadcastAddressValid()
+    public function testIsBroadcastAddressValid(): void
     {
         $this->assertTrue(PHPWakeOnLan::isBroadcastAddressValid('192.168.1.255'));
         $this->assertFalse(PHPWakeOnLan::isBroadcastAddressValid('192.168.1.33'));
+    }
+
+    /**
+     * @covers \Diegonz\PHPWakeOnLan\PHPWakeOnLan::wake()
+     * @throws \Exception
+     */
+    public function testWake(): void
+    {
+        $wol = new PHPWakeOnLan();
+        $result = $wol->wake(['00:1B:2C:1C:DF:22']);
+
+        //$this->assertTrue(is_array($result));
+        $this->assertNotEmpty($result);
     }
 }
