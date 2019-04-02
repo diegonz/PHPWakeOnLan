d8207256184fc1f295000df37d6cee4265c927c7
Diego <dgonzalez.BZ@gmail.com>
2019-04-02 23:03:33 +0200

    Apply fixes from StyleCI (#7)

--- config/php-wake-on-lan.php
+++ config/php-wake-on-lan.php
@@ -5,12 +5,12 @@
  */
 return [
 
-    /**
+    /*
      * String containing target broadcast address in XXX.XXX.XXX.255 ipv4 format
      */
     'broadcast_address' => '255.255.255.255',
 
-    /**
+    /*
      * Integer containing target port to use. Must be 7 or 9.
      */
     'port'              => 7,
